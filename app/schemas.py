3f6c9644cfa5829c9a4225e3588ee991784cbde1
Onichan <bryankoh17102@gmail.com>
2022-03-28 14:21:25 +0800

    delete duplicate UserOut schema in schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -41,14 +41,6 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
-class UserOut(BaseModel):
-    email: EmailStr
-    id: int
-    created_at: datetime
-
-    class Config:
-        orm_mode = True
-
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
